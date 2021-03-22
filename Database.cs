f057647dfaead8067d37f6a6888ee50db13609b7
VidBerlocnik <vid.berlocnik@scv.si>
2021-03-22 14:00:05 +0100

    Added database connection properties and changed NuGet Package to System.Data.SQLite.

--- Database.cs
+++ Database.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Sql;
+using System.Data.SQLite;
 
 namespace LibraryProject
 {
     class Database
     {
-        
+        private SQLiteConnection conn;
+
+        //Constructor
+        public Database()
+        {
+            conn = new SQLiteConnection("data source=items.db");
+        }
     }
 }

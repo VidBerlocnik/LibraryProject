9ad18b27c184943a135a91c88c87c1d9f63745f0
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-04-06 14:06:24 +0200

    Dodal class za lazjo obdelavo podatkov

---
+++ Knjiga.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    class Knjiga
+    {
+        public int id { get; set; }
+        public string naslov { get; set; }
+        public string leto_izdaje { get; set; }
+        public bool izgubljena { get; set; }
+        public bool trgovina { get; set; }
+        public string opombe { get; set; }
+
+        public Knjiga()
+        {
+        }
+
+        public Knjiga(int id, string naslov, string leto_izdaje, bool izgubljena, bool trgovina)
+        {
+            this.id = id;
+            this.naslov = naslov;
+            this.leto_izdaje = leto_izdaje;
+            this.izgubljena = izgubljena;
+            this.trgovina = trgovina;
+        }
+
+        public Knjiga(int id, string naslov, string leto_izdaje)
+        {
+            this.id = id;
+            this.naslov = naslov;
+            this.leto_izdaje = leto_izdaje;
+        }
+    }
+}
---
+++ Uporabniki.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    class Uporabniki
+    {
+        public int id { get; set; }
+        public string ime { get; set; }
+        public string priimek { get; set; }
+        public string telefon { get; set; }
+        public string naslov { get; set; }
+        public string email { get; set; }
+        public string opombe { get; set; }
+        public Uporabniki()
+        {
+        }
+
+        public Uporabniki(int id, string ime, string priimek)
+        {
+            this.id = id;
+            this.ime = ime;
+            this.priimek = priimek;
+        }
+
+        public override string ToString()
+        {
+            return this.ime + " " + this.priimek;
+        }
+    }
+}

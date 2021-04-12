1eab6baef168e875c9c5473effa4fa4f614355ed
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-12 16:33:13 +0200

    Lastnost avtor spremenil na dve lastnosti (ime, priimek).

--- Gradivo.cs
+++ Gradivo.cs
@@ -13,21 +13,23 @@ namespace LibraryProject
         public int Id { get; set; }
         public string Naslov { get; set; }
         public string LetoIzdaje { get; set; }
-        public string Avtor { get; set; }
+        public string ImeAvtorja { get; set; }
+        public string PriimekAvtorja { get; set; }
 
         //Constructor
-        public Gradivo(int id, string naslov, string letoIzdaje, string avtor)
+        public Gradivo(int id, string naslov, string letoIzdaje, string ime, string priimek)
         {
             Id = id;
             Naslov = naslov;
             LetoIzdaje = letoIzdaje;
-            Avtor = avtor;
+            ImeAvtorja = ime;
+            PriimekAvtorja = priimek;
         }
 
         //functions
         public override string ToString()
         {
-            return Id.ToString() + " " + Naslov + " " + Avtor + " " + LetoIzdaje;
+            return Id.ToString() + " " + Naslov + " " + ImeAvtorja + " " + PriimekAvtorja + " " + LetoIzdaje;
         }
     }
 }

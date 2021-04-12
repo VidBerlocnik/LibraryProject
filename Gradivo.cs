a914469791f7be27cafeeeb5d2803ee1f6b17b1a
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-12 16:17:51 +0200

    Ustvaril nov class Gradivo

---
+++ Gradivo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    class Gradivo
+    {
+        //properties
+
+        public int Id { get; set; }
+        public string Naslov { get; set; }
+        public string LetoIzdaje { get; set; }
+        public string Avtor { get; set; }
+
+        //functions
+        public override string ToString()
+        {
+            return Id.ToString() + " " + Naslov + " " + Avtor + " " + LetoIzdaje;
+        }
+    }
+}
